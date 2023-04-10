7e07585e13d2d853076a91d90f155f2cadb20e6f
Hipox <hipoxicek@gmail.com>
2023-04-10 16:45:04 +0200

    Update Hipox_Hello world.lua

--- Example/Hipox_Hello world.lua
+++ Example/Hipox_Hello world.lua
@@ -1,6 +1,6 @@
 -- @description Hello world
 -- @author Hipox
--- @version 1.0
+-- @version 1.1
 -- @about
 --   This is an example of a package file. It installs itself as a ReaScript that
 --   does nothing but show "Hello World!" in REAPER's scripting console.
